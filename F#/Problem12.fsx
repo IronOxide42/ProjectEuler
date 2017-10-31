a8b181dcbe58a8a327baae18e6aa85b2491896ce
Jordan Rust <jrust@securustechnologies.com>
2017-10-31 16:38:52 -0500

    Refactor with pipe operators

--- F#/Problem12.fsx
+++ F#/Problem12.fsx
@@ -5,14 +5,14 @@ let rec GetTriangleNumber (index:bigint) =
 let GetDivisors dividend =
     let rec getDivisorsHelper (dividend:bigint) (curDivisor:bigint) =
         if curDivisor = (bigint 1) then [(bigint 1);]
-        elif (dividend % curDivisor = (bigint 0)) then curDivisor::(getDivisorsHelper dividend (curDivisor-(bigint 1)))
+        elif (dividend % curDivisor = (bigint 0)) then curDivisor::(getDivisorsHelper dividend (curDivisor - (bigint 1)))
         else getDivisorsHelper dividend (curDivisor-(bigint 1))
     getDivisorsHelper dividend dividend
 
 let rec GetTriangleNumberWithOver divisorCount =
     let rec getTriangleNumberWithOverHelper (divisorCount:bigint) (index:bigint) =
-        if bigint(Seq.length (GetDivisors (GetTriangleNumber index))) > divisorCount then (GetTriangleNumber index)
-        else getTriangleNumberWithOverHelper divisorCount (index+(bigint 1))
+        if bigint(index |> GetTriangleNumber |> GetDivisors |> Seq.length)  > divisorCount then (GetTriangleNumber index)
+        else getTriangleNumberWithOverHelper divisorCount (index + (bigint 1))
     getTriangleNumberWithOverHelper divisorCount (bigint 1)
 
 GetTriangleNumberWithOver (bigint 500);;
