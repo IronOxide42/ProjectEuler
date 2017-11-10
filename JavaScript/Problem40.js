4d9ed254589a287ba553efbc03052b5d9904e659
Jordan Rust <jrust@securustechnologies.com>
2017-11-10 15:30:38 -0600

    Complete Problem 40

---
+++ JavaScript/Problem40.js
@@ -0,0 +1,14 @@
+let str = "0"
+let product = 1
+
+//If the log base 10 of n is an integer, n is a power of 10
+let isPowerOf = (n,base) => Number.isInteger(Math.log2(n) / Math.log2(base))
+
+for(let i = 1; i <= 1000000; ++i) {
+    str += i
+    if(isPowerOf(i,10)) { 
+        product *= str.charAt(i)
+    }
+}
+
+console.log(product)
