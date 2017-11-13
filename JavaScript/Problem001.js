f483ce86dd204d1568c26748556d546cde138ac1
Jordan Rust <jrust@securustechnologies.com>
2017-11-13 17:18:06 -0600

    Change ForEach to Reduce

--- JavaScript/Problem001.js
+++ JavaScript/Problem001.js
@@ -11,13 +11,10 @@ let problem1 = value => {
     const divisors = [ 3, 5 ];
 
     for(let i = 0; i < max; i++) {
-        let qualifies = false;
-        divisors.forEach(
-            divisor => {
-                if(IsMultipleOf(i, divisor)) { qualifies = true; }
-            }
-        );
-        if (qualifies) { sum += i; }
+        //If at least one item in divisors divides i
+        if(divisors.reduce((prev, cur) => (prev || (IsMultipleOf(i, cur))), false)) {
+            sum += i;
+        }
     }
     return sum;
 };
