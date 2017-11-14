a2246dd8070ff167513a7cef9096d15d32b3245e
Jordan Rust <IronOxide42@gmail.com>
2017-11-14 17:15:31 -0600

    Convert to function

--- JavaScript/Problem010.js
+++ JavaScript/Problem010.js
@@ -1,31 +1,25 @@
-/*
-The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+let problem10 = maxExclusive => {
+    let IsPrime = number => {
+        if (number <= 1) {return false;}
+        if (number == 2) {return true;}
+        if (number % 2 == 0) {return false;}
 
-Find the sum of all the primes below two million.
-*/
+        let boundary = Math.floor(Math.sqrt(number));
 
-//https://stackoverflow.com/a/15743238
-let IsPrime = number => {
-    if (number <= 1) {return false;}
-    if (number == 2) {return true;}
-    if (number % 2 == 0) {return false;}
+        for (let i = 3; i <= boundary; i += 2) {
+            if (number % i == 0) {return false;}
+        }
 
-    let boundary = Math.floor(Math.sqrt(number));
+        return true;
+    };
 
-    for (let i = 3; i <= boundary; i += 2) {
-        if (number % i == 0) {return false;}
+    let value = 0;
+
+    for(let i = 0; i <= maxExclusive; ++i) {
+        if(IsPrime(i)) {
+            value += i;
+        }
     }
 
-    return true;
-};
-
-let value = 0;
-
-for(let i = 0; i <= 2000000; ++i) {
-    if(IsPrime(i)) {
-        value += i;
-    }
+    return value;
 }
-
-console.log(value);
-//142913828922
